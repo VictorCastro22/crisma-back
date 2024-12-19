a2c02cb6d0038e83b61fcb40c11e58d7ce326461
Victor Manuel <victor3mcastro@gmail.com>
2024-12-19 14:31:49 -0300

    ATT

--- src/server.ts
+++ src/server.ts
@@ -70,24 +70,12 @@ app.get('/reports', (req: Request, res: Response) => {
     });
 });
 
-app.get('/update-null-values', (req: Request, res: Response) => {
-    db.run('UPDATE attendance SET turma = "Turma desconhecida" WHERE turma IS NULL');
-    db.run('UPDATE attendance SET crismando = "Crismando desconhecido" WHERE crismando IS NULL');
-    db.run('UPDATE attendance SET presenca = "Presenca desconhecida" WHERE presenca IS NULL');
-    db.run('UPDATE attendance SET catequista = "Catequista desconhecido" WHERE catequista IS NULL', [], (err) => {
+app.delete('/delete-all', (req: Request, res: Response) => {
+    db.run('DELETE FROM attendance', [], (err) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.json({ message: 'Valores null atualizados!' });
-    });
-});
-
-app.delete('/delete-null-entries', (req: Request, res: Response) => {
-    db.run('DELETE FROM attendance WHERE turma IS NULL OR crismando IS NULL OR presenca IS NULL OR catequista IS NULL', [], (err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json({ message: 'Registros com valores null foram excluídos!' });
+        res.json({ message: 'Todos os registros foram excluídos!' });
     });
 });
 
